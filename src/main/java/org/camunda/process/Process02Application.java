d5469dad95802ae8472bfecd225c8560ced5c2e6
Prasad <prasadprabha07@gmail.com>
2021-05-16 17:38:57 +0530

    Initial Commit

---
+++ src/main/java/org/camunda/process/Process02Application.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.process;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Process 02 App")
+public class Process02Application extends ServletProcessApplication {
+
+}
